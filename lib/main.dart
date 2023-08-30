55290e8fd6029656349c94ee38e47d385a8a447d
RobertoLunaSalas <lunasalasr@gmail.com>
2023-08-29 20:23:16 -0600

    Version Final

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,13 @@ class _ColorScreenState extends State<ColorScreen> {
               Navigator.of(context).pushNamed(
                 '/edit',
                 arguments: _color,
-              );
+              ).then((result){
+                  if(result != null){
+                    setState((){
+                      _color = result as Color;
+                    });
+                  }
+              });
             },
           ),
         ),
@@ -56,18 +62,20 @@ class _EditColorScreenState extends State<EditColorScreen> {
 
   @override
   void didChangeDependencies() {
-    final Color color = ModalRoute.of(context).settings.arguments;
-    final List<String> canales = [
-      color.red.toString(),
-      color.green.toString(),
-      color.blue.toString(),
-    ];
-    _controllers = [
-      for (int i = 0; i < 3; i++)
-        TextEditingController(
-          text: canales[i],
-        ),
-    ];
+    final Color? color = ModalRoute.of(context)?.settings.arguments as Color?;
+    if (color != null) {
+      final List<String> canales = [
+        color.red.toString(),
+        color.green.toString(),
+        color.blue.toString(),
+      ];
+      _controllers = [
+        for (int i = 0; i < 3; i++)
+          TextEditingController(
+            text: canales[i],
+          ),
+      ];
+    }
     super.didChangeDependencies();
   }
 
@@ -75,34 +83,41 @@ class _EditColorScreenState extends State<EditColorScreen> {
   Widget build(BuildContext context) {
     const List<String> colores = ['red', 'green', 'blue'];
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Editar Color'),
+      appBar: AppBar(
+        title: Text('Edit Color'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                for (int i = 0; i < 3; i++)
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: TextField(
+                        controller: _controllers[i],
+                        decoration: InputDecoration(
+                          labelText: colores[i],
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            ElevatedButton(
+              child: Text('Guardar'),
+              onPressed: () {
+                final int r = int.parse(_controllers[0].text);
+                final int g = int.parse(_controllers[1].text);
+                final int b = int.parse(_controllers[2].text);
+                Navigator.of(context).pop(Color.fromARGB(255, r, g, b));
+              },
+            ),
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: Column(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  for (int i = 0; i < 3; i++)
-                    Expanded(
-                      child: Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: TextField(
-                            controller: _controllers[i],
-                            decoration: InputDecoration(
-                              labelText: colores[i],
-                            ),
-                          )),
-                    )
-                ],
-              ),
-              ElevatedButton(
-                child: Text('Guardar'),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
