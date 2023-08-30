e33788ad2aa2f1e65899684684973de6a6f35fd5
sergiomartinez-29 <sergio2962002@gmail.com>
2023-08-29 19:47:24 -0600

    min 15

--- lib/main.dart
+++ lib/main.dart
@@ -24,21 +24,24 @@ class _ColorScreenState extends State<ColorScreen> {
   Color _color = Color.fromARGB(255, 255, 0, 0);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: _color,
-        body: Padding(
-          padding: EdgeInsets.all(20),
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: ElevatedButton(
-              child: Text('Cambiar Color'),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/edit');
-              },
-            ),
+      backgroundColor: _color,
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: ElevatedButton(
+            child: Text('Cambiar Color'),
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                '/edit',
+                arguments: _color,
+              );
+            },
           ),
         ),
+      ),
     );
   }
 }
@@ -52,47 +55,54 @@ class _EditColorScreenState extends State<EditColorScreen> {
   late List<TextEditingController> _controllers;
 
   @override
-  void initState(){
-    _controllers = [
-      for(int i = 0; i < 3; i++) TextEditingController(),
+  void didChangeDependencies() {
+    final Color color = ModalRoute.of(context).settings.arguments;
+    final List<String> canales = [
+      color.red.toString(),
+      color.green.toString(),
+      color.blue.toString(),
     ];
-    super.initState();
+    _controllers = [
+      for (int i = 0; i < 3; i++)
+        TextEditingController(
+          text: canales[i],
+        ),
+    ];
+    super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
-    const List<String> colores = ['Rojo', 'Verde', 'Azul'];
+    const List<String> colores = ['red', 'green', 'blue'];
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Editar Color'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        child: Column(
-        children: <Widget>[
-          Row(
+        appBar: AppBar(
+          title: Text('Editar Color'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
             children: <Widget>[
-              for(int i = 0; i < 3; i++)
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: TextField(
-                    controller: _controllers[i],
-                    decoration: InputDecoration(
-                      labelText: colores[i],
-                    ),
-                  )
-                  ),  
-                )
+              Row(
+                children: <Widget>[
+                  for (int i = 0; i < 3; i++)
+                    Expanded(
+                      child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: TextField(
+                            controller: _controllers[i],
+                            decoration: InputDecoration(
+                              labelText: colores[i],
+                            ),
+                          )),
+                    )
+                ],
+              ),
+              ElevatedButton(
+                child: Text('Guardar'),
+                onPressed: () {},
+              ),
             ],
           ),
-          ElevatedButton(
-            child: Text('Guardar'),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      )
-    );
+        ));
   }
 }
